673b4d3208df4bcbfbe65a41f44bd7e1978070f0
Emanuel Alderete <emanuel-alderete@hotmail.com>
2022-02-03 12:20:25 -0300

    se agrega explicacion de uso del objeto Date para trabajar con fechas

---
+++ js/38-fechas.js
@@ -0,0 +1,20 @@
+let fecha = new Date();//objeto de fechas
+
+let year = fecha.getFullYear();//obtenemos año completo
+let mes = fecha.getMonth();//tener en cuenta que el mes enero es igual a cero, es decir empieza a contar desde cero
+let dia = fecha.getDate();//obtengo el dia
+ // En JavaScript los meses van desde el cero (Enero) a once (Diciembre),
+ //  los días de la semana del cero (Domingo) a seis (Sábado).
+let hora = fecha.getHours();
+let minutos = fecha.getMinutes();
+let segundos = fecha.getSeconds();
+let textoHora =`
+	El año es: ${year}
+	El mes es: ${mes}
+	El dia es: ${dia}
+	La hora es: ${hora}
+	Los minutos son: ${minutos}
+	Los segundos son: ${segundos}
+`;
+console.log(fecha);
+console.log(textoHora);
